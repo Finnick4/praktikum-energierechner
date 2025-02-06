6eb147621de1f3a92fbb23598696389394671efc
finnick4 <finnick4gallfeder@gmail.com>
2025-02-06 11:24:09 +0100

    Added first pipe

--- Main.py
+++ Main.py
@@ -1,12 +1,13 @@
 from GetEnergy import *
 import json
 
-x = GetEnergy.GetEnergyCharts()
+ec = EnergyCharts()
+
+x = ec.getPrice()
 
 count = 0
 print("Zeit\t| EnergyCharts Preis")
-for value in x["price"]:
+for value in x:
     print(f"{count}\t| {value}")
     count += 1
 
-x = Energy()
