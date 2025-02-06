5f3f0420e5b2f098657cdd1a1aeb394529dfaa12
finnick4 <finnick4gallfeder@gmail.com>
2025-02-06 11:55:03 +0100

    Pipeline Test

--- Main.py
+++ Main.py
@@ -1,9 +1,18 @@
+import datetime
 from GetEnergy import *
-import json
 
 ec = EnergyCharts()
 
 x = ec.getPrice()
+print(datetime.datetime.now().hour)
+
+# Wichtig: Preis in Eur / MWh
+
+currentPrice = x[datetime.datetime.now().hour - 1]
+
+if (currentPrice < 146):
+    print("Der Strom ist unter 146 EUR/MWh")
+    exit(1)
 
 count = 0
 print("Zeit\t| EnergyCharts Preis")
