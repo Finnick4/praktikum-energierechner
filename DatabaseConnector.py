abc6c719af44e44e8abda09e8379d24f90835243
finnick4 <finnick4gallfeder@gmail.com>
2025-02-11 14:10:31 +0100

    [DB] Movement to package dotenv v1.0.1 from v0.0.1

--- DatabaseConnector.py
+++ DatabaseConnector.py
@@ -1,22 +1,20 @@
 import mysql.connector
 import dotenv 
-from abc import ABC, abstractmethod
+from abc import ABC
 
 
 class db(ABC):
     # !!! Currently only for price !!!
     def __init__(self):
-        __env__ = dotenv.DotEnv()
-        """self.__db__ = mysql.connector.connect(
+        self.__db__ = mysql.connector.connect(
             host="localhost",
-            user=__env__.get("USER"),
-            password=__env__.get("PASSWORD"),
-            database=__env__.get("DATABASE")
-        )"""
-
+            user=dotenv.get_key(dotenv.find_dotenv(), "USER"),
+            password=dotenv.get_key(dotenv.find_dotenv(), "PASSWORD"),
+            database=dotenv.get_key(dotenv.find_dotenv(), "DATABASE"),
+        )
         self._sql_ = ""
         self.__stack__ = []
-        # self.__cursor__ = self.__db__.cursor()
+        self.__cursor__ = self.__db__.cursor()
 
 
     def addToStack(self, toAdd):
@@ -24,13 +22,17 @@ class db(ABC):
         self.__stack__.append(toAdd)
     
     def sendStack(self):
-        print(self._sql_, self.__stack__)
-        #self.__cursor__.executemany(self.__sql__, self.__stack__)
+        #print(self._sql_, self.__stack__)
+        self.__cursor__.executemany(self.__sql__, self.__stack__)
+
+
 
 class priceDB(db):
     def __init__(self):
         super().__init__()
         self._sql_ = "INSERT INTO price (epoch, eur) VALUES (%s, %s)"
+
+
 
 class freqDB(db):
     def __init__(self):
